c4b535ca13f795ff198afa172c0954b2b53dc408
Anna Lopez <annalopezribo@gmail.com>
2022-03-05 14:34:53 +0100

    Style fixed for 100% size of explorer

--- js/app.js
+++ js/app.js
@@ -12,14 +12,14 @@ const getArtists = async () => {
       var card = '<div class="card"><img class="card-img-top" src="';
       card += artists[i].photo;
       card += '"alt="Image of ';
-      card+= artists[i].name+'"Card image cap"><div class="card-body"><h3 class="card-title">';
+      card+= artists[i].name+'"Card image cap"><div class="card-body"><h5 class="card-title">';
       card += "<a class='artist_uri' href='"+apiURLSingleArtist+artists[i].uri+"' target='_blank' >"+artists[i].name+"</a>";
-      card += '</h3><span class="card-text">';
-      card += '<h5><i>'+artists[i].genre+'</i> · '+commarize(artists[i].listeners)+' Listeners</h5>';
+      card += '</h5><span class="card-text">';
+      card += '<h6><i>'+artists[i].genre+'</i> · '+commarize(artists[i].listeners)+' Listeners</h6>';
       card +=  '<p class="artist-info">'+artists[i].info+'</p>';
-      card += '<h5>Albums</h5><p class="artist-albums">'+addSpace(artists[i].albums)+'</p>';
-      card += '<h5>Top tracks</h5><p class="top-tracks">'+addSpace(artists[i].top_tracks)+'</p>';
-      card += '<h5>Similar Artists</h5><p class="top-tracks">'+addSpace(artists[i].similar_to)+'</p>';
+      card += '<h6>Albums</h6><p class="artist-albums">'+addSpace(artists[i].albums)+'</p>';
+      card += '<h6>Top tracks</h6><p class="top-tracks">'+addSpace(artists[i].top_tracks)+'</p>';
+      card += '<h6>Similar Artists</h6><p class="top-tracks">'+addSpace(artists[i].similar_to)+'</p>';
       card += '</span></div></div>'; //card body end
       content.innerHTML+=card;
   }
